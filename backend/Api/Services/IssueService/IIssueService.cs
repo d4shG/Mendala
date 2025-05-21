e1cd6da8e372d00890f286d834b38759818a7e22
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-05-21 15:00:19 +0200

    Added Interface methods

--- backend/Api/Services/IssueService/IIssueService.cs
+++ backend/Api/Services/IssueService/IIssueService.cs
@@ -1,6 +1,15 @@
+using Api.Models.Enums;
+using Api.Models.IssueDtos;
+
 namespace Api.Services.IssueService;
 
 public interface IIssueService
 {
-	
+	Task<List<IssueResponseDto>> GetAllAsync();
+	Task<IssueResponseDto> GetByIdAsync(Guid id);
+	Task<Guid> CreateAsync(IssueCreateDto issue);
+	Task UpdateAsync(IssueUpdateDto issue);
+	Task DeleteAsync(Guid id);
+	Task<List<IssueResponseDto>> GetByFilterAsync(IssueFilterDto filter);
+	Task UpdateStatusAsync(Guid id, IssueStatusUpdateDto status);
 }
