5831867e656f7c241a6f264624d5c9c88df8dbdd
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-05-21 15:00:34 +0200

    Added methods to controller

--- backend/Api/Controllers/IssueController/IssueController.cs
+++ backend/Api/Controllers/IssueController/IssueController.cs
@@ -1,39 +1,68 @@
 using Api.Data.Entities;
+using Api.Models.IssueDtos;
+using Api.Services.IssueService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers.IssueController;
 
 [ApiController]
 [Route("[controller]")]
-public class IssueController : ControllerBase
+public class IssueController(IIssueService issueService) : ControllerBase
 {
 	[HttpGet]
 	public async Task<IActionResult> GetIssues()
 	{
-		throw new NotImplementedException();
+		var issues = await issueService.GetAllAsync();
+		
+		return Ok(issues);
 	}
 
 	[HttpGet("{id}")]
 	public async Task<IActionResult> GetIssueById(Guid id)
 	{
-		throw new NotImplementedException();
+		var issue = await issueService.GetByIdAsync(id);
+		
+		if (issue == null)
+			return NotFound();
+		
+		return Ok(issue);
 	}
 
 	[HttpPost]
-	public async Task<IActionResult> Create([FromBody] IssueCreateDTo issue)
+	public async Task<IActionResult> Create([FromBody] IssueCreateDto issue)
 	{
-		throw new NotImplementedException();
+		var createdIssueId = await issueService.CreateAsync(issue);
+
+		return CreatedAtAction(nameof(GetIssueById), new { id = createdIssueId });
 	}
 
 	[HttpPut]
 	public async Task<IActionResult> Update([FromBody] IssueUpdateDto issue)
 	{
-		throw new NotImplementedException();
+		await issueService.UpdateAsync(issue);
+
+		return NoContent();
 	}
 
 	[HttpDelete("{id}")]
-	public IActionResult Delete(Guid id)
+	public async Task<IActionResult> Delete(Guid id)
 	{
-		throw new NotImplementedException();
+		await issueService.DeleteAsync(id);
+
+		return NoContent();
+	}
+	
+	[HttpGet("filter")]
+	public async Task<IActionResult> GetIssuesByFilter([FromQuery] IssueFilterDto filter)
+	{
+		var issues = await issueService.GetByFilterAsync(filter);
+		return Ok(issues);
+	}
+	
+	[HttpPatch("{id}/status")]
+	public async Task<IActionResult> UpdateStatus(Guid id, [FromBody] IssueStatusUpdateDto statusUpdateDto)
+	{
+		await issueService.UpdateStatusAsync(id, statusUpdateDto);
+		return Ok();
 	}
 }
