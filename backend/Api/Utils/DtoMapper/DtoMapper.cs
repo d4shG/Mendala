2898691b9a2919590ad80e57a585adf8cc82940b
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2025-05-30 17:20:10 +0200

    Added dtomapper static class

---
+++ backend/Api/Utils/DtoMapper/DtoMapper.cs
@@ -0,0 +1,69 @@
+using Api.Data.Entities;
+using Api.Models.CustomerDtos;
+using Api.Models.InvoiceDtos;
+using Api.Models.InvoiceItemDtos;
+using Api.Models.IssueDtos;
+using Api.Models.ProductDtos;
+
+namespace Api.Utils.DtoMapper;
+
+public static class DtoMapper
+{
+	public static ProductResponseDto ConvertProductToProductResponseDto(Product product) =>
+		new ProductResponseDto(product.Id,
+			product.Name,
+			product.Sku
+		);
+	
+	public static IssueResponseDto ConvertIssueToIssueResponseDto(Issue issue) =>
+		new IssueResponseDto(
+			issue.Id,
+			issue.Title,
+			issue.Description,
+			issue.CreatorId,
+			issue.Creator.UserName ?? "Admin",
+			issue.InvoiceId,
+			issue.Invoice.InvoiceNumber,
+			issue.Invoice.CustomerId,
+			issue.Invoice.Customer.Name,
+			issue.Status,
+			issue.Notes ?? null,
+			issue.CreatedAt
+		);
+	
+	public static CustomerResponseDto ConvertCustomerToCustomerResponseDto(Customer customer) =>
+		new CustomerResponseDto(customer.Id, 
+			customer.Name, 
+			customer.Email, 
+			customer.Phone, 
+			customer.Address);
+
+	private static InvoiceItemResponseDto ConvertInvoiceItemToInvoiceItemResponseDto(InvoiceItem item)
+	{
+		var product = ConvertProductToProductResponseDto(item.Product);
+		
+		return new InvoiceItemResponseDto(item.Id,
+			product,
+			item.PriceAtPurchase,
+			item.Quantity
+		);
+	}
+	
+	public static InvoiceResponseDto ConvertInvoiceToInvoiceResponseDto(Invoice invoice)
+	{
+		var items = invoice.InvoiceItems.Select(ConvertInvoiceItemToInvoiceItemResponseDto);
+		
+		return new InvoiceResponseDto(invoice.Id,
+			invoice.InvoiceNumber,
+			invoice.Name,
+			invoice.Address,
+			invoice.CustomerId,
+			items
+			);
+	}
+	
+	
+
+	
+	
+}
